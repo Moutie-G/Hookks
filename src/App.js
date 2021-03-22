a41538b23211d57988ca8182360a53b6b7f284d2
Moutie-G <moutiechaabeni8@gmail.com>
2021-03-22 17:39:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {movies} from './assets/Data'
+import MovieList from './components/MovieList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [listMovie, setListMovie] = useState(movies)
+  const addFilm=(form)=>{
+    //form={title,posterUrl,description,rate}
+    setListMovie([form,...listMovie])
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid' >
+      <MovieList listMovie={listMovie} addFilm={addFilm} />
     </div>
   );
 }
---
+++ src/HooksState.js
@@ -0,0 +1,13 @@
+import recat,{useEffect, UseState} from "react" ;
+
+
+
+
+function useInterval(timeOut, getValue){
+    const [avlue, SetValue] = useState(getValue);
+    useEffect(()=>{
+        const intervallId=SetInterval(()
+    }
+
+    )
+}
